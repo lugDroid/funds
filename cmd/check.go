dfc6eb7e024d2829be22510cb9e169a04f3d12b6
lugDroid <diazbastos@gmail.com>
2022-04-08 16:36:02 +0200

    Fix info output format

--- cmd/check.go
+++ cmd/check.go
@@ -13,12 +13,16 @@ var checkCmd = &cobra.Command{
 	Args:  cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("Retrieving stock/fund information...")
+		fmt.Println()
+
 		data := yahoofinanceapi.GetFundData(args[0])
 
 		if len(data.QuoteResponse.Result) > 0 {
-			fmt.Println(data.QuoteResponse.Result[0].LongName)
-			fmt.Println(data.QuoteResponse.Result[0].RegularMarketPrice)
-			fmt.Println(data.QuoteResponse.Result[0].RegularMarketChangePercent)
+			result := data.QuoteResponse.Result[0]
+			fmt.Println("Name:\t\t", result.LongName)
+			fmt.Println("Price:\t\t", result.RegularMarketPrice, result.Currency)
+			fmt.Printf("Change:\t\t %.2f %%\n", data.QuoteResponse.Result[0].RegularMarketChangePercent)
+			fmt.Printf("YTD Return:\t %.2f %%\n", data.QuoteResponse.Result[0].YtdReturn)
 		} else {
 			fmt.Println("Ticker not valid")
 		}
