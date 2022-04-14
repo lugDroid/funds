871c0d0a38d22a49e83f452d1f09f36ed0ad1f88
lugDroid <diazbastos@gmail.com>
2022-04-14 08:47:47 +0200

    Add category information

--- cmd/list.go
+++ cmd/list.go
@@ -17,9 +17,20 @@ var listCmd = &cobra.Command{
 		storedFunds := data.ReadFundsFile()
 
 		for _, fund := range storedFunds.Funds {
-			fmt.Println("Fund:\t", fund.FundName)
-			fmt.Println("Ticker:\t", fund.Ticker)
-			fmt.Println("Shares:\t", fund.Shares)
+			var categoryName string
+			for _, c := range storedFunds.AssetsAllocation {
+				if fund.AssetId == c.Id {
+					categoryName = c.Name
+					break
+				}
+
+				categoryName = "Not Assigned"
+			}
+
+			fmt.Println("Fund:\t\t", fund.FundName)
+			fmt.Println("Ticker:\t\t", fund.Ticker)
+			fmt.Println("Category:\t", categoryName)
+			fmt.Println("Shares:\t\t", fund.Shares)
 			fmt.Println()
 		}
 	},
